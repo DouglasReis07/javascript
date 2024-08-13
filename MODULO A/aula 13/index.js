c43b0de747cc7009856d5450893234bbb0246c94
DouglasReis07 <163474683+DouglasReis07@users.noreply.github.com>
2024-08-13 14:16:52 -0300

    repetiçoes

---
+++ MODULO A/aula 13/index.js
@@ -0,0 +1,23 @@
+var c = 1
+do {
+    console.log(`Botelho me deve ${c}`)
+    c++
+} while (c <=6)
+
+
+/*
+var c = 1
+while (c <= 10) {
+    console.log(`Botelho me deve ${c}`)
+    c++
+}
+*/
+
+/*
+console.log('Botelho me deve 100 real')
+console.log('Botelho me deve 100 real')
+console.log('Botelho me deve 100 real')
+console.log('Botelho me deve 100 real')
+console.log('Botelho me deve 100 real')
+console.log('Botelho me deve 100 real')
+*/
